e36aeda754492742e75a5ff2d537136544e3c864
Sparks-design-lab <150973312+Sparks-design-lab@users.noreply.github.com>
2023-11-21 10:34:43 +0000

    Add files via upload

--- app/routes.js
+++ app/routes.js
@@ -6,20 +6,20 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
+// Run this code when a form is submitted to 'questions-answer'
+router.post('/questions-answer', function (req, res) {
 
-// Run this code when a form is submitted to 'check-answers'
-router.post('/check-answers', function (req, res) {
-
-    // Make a variable and give it the value from 'other'
-    var whichDestination= req.session.data['which-destination']
+    // Make a variable and give it the value from 'which-destination'
+    var whichDestination = req.session.data['which-destination']
   
     // Check whether the variable matches a condition
-    if (whichDestination == "other"){
+    if (whichDestination == "mars"){
       // Send user to next page
-      res.redirect('/ineligible')
+      res.redirect('/name')
     } else {
       // Send user to ineligible page
-      res.redirect('/name')
+      res.redirect('/ineligible')
     }
+    
   
   })
